065c53977cb5bceaaac9276d290000a9ce2b81ad
Stivvo <stivvo01@gmail.com>
2020-06-30 18:37:48 +0200

    more directories

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chmod +x "${PWD}/wrapWayPclient.sh"
-chmod +x "${PWD}/wrapWayPserver.sh"
-sudo ln -sf "${PWD}/../clientBuild/wayPreviewClient" /usr/bin/wayPreviewClient
-sudo ln -sf "${PWD}/../serverBuild/wayPreviewServer" /usr/bin/wayPreviewServer
-sudo ln -sf "${PWD}/wrapWayPclient.sh" /usr/bin/wrapWayPclient.sh
-sudo ln -sf "${PWD}/wrapWayPserver.sh" /usr/bin/wrapWayPserver.sh
