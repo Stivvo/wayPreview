e80fa263bdeaca0afc3951610613a45c85e7b100
Stivvo <stivvo01@gmail.com>
2020-07-15 17:23:36 +0200

    fix install config

--- scripts/installConfig.sh
+++ scripts/installConfig.sh
@@ -3,8 +3,8 @@
 WAYPCONF="${XDG_CONFIG_HOME:=${HOME}/.config}/wayPreview/"
 mkdir -p $WAYPCONF
 
-cp "${PWD}/../config/serverConf.sh" $WAYPCONF
-cp "${PWD}/../config/clientConf.sh" $WAYPCONF
-cp "${PWD}/../config/setPosConf.sh" $WAYPCONF
+cp "${PWD}/../config/wrapWayPclient.sh" $WAYPCONF
+cp "${PWD}/../config/wrapWayPserver.sh" $WAYPCONF
+cp "${PWD}/../config/wrapSetPos.sh" $WAYPCONF
 
 echo "configuration files in $WAYPCONF"
