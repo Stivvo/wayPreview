60604370e27832be95b45318cfdaba7e8bac6c23
Stivvo <stivvo01@gmail.com>
2020-08-24 14:10:08 +0200

    universal pkg: update, run *stall.sh once
    createPackage.sh has been updated to the new changes in the directory
    structure
    
    Uses multiline echo, globs and doesn't explicitly specifies the output
    file name to make the script shorter
    
    *stall.sh in the root dir of the universal package run the matching
    *stall.sh twice (once for system files, once for user files)

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -10,29 +10,26 @@ cd "../../$RELEASEDIR"
 
 # copying binaries
 mkdir clientBuild serverBuild
-cp ../clientBuild/wayPreviewClient clientBuild/wayPreviewClient
-cp ../serverBuild/wayPreviewServer serverBuild/wayPreviewServer
+cp ../clientBuild/wayPreviewClient clientBuild/
+cp ../serverBuild/wayPreviewServer serverBuild/
 
 # copying scripts
-mkdir wayPreview wayPreview/scripts
-cp ../wayPreview/scripts/install.sh wayPreview/scripts/install.sh
-cp ../wayPreview/scripts/installConfig.sh wayPreview/scripts/installConfig.sh
-cp ../wayPreview/scripts/uninstall.sh uninstall.sh
+mkdir -p wayPreview/scripts
+cp ../wayPreview/scripts/*stall.sh wayPreview/scripts/
 
 # copying config files and documentation
-cp -r ../wayPreview/config wayPreview/
 cp -r ../wayPreview/wrappers wayPreview/
-cp ../wayPreview/README.md wayPreview/README.md
+cp ../wayPreview/README.md wayPreview/
 
 # creating install.sh that runs wayPreview/scripts/install.sh
 echo "#!/bin/sh" > install.sh
-echo "cd wayPreview/scripts/" >> install.sh
-echo "./install.sh install program" >> install.sh
+echo "cd wayPreview/scripts/ && sudo ./install.sh install program &&
+./install.sh install wrappers" >> install.sh
 
-# creating installConfig.sh that runs wayPreview/scripts/installConfig.sh
-echo "#!/bin/sh" > installConfig.sh
-echo "cd wayPreview/scripts/" >> installConfig.sh
-echo "./install.sh install config" >> installConfig.sh
+# creating uninstall.sh that runs wayPreview/scripts/uninstall.sh
+echo "#!/bin/sh" > uninstall.sh
+echo "cd wayPreview/scripts/ && sudo ./uninstall.sh program
+./uninstall.sh wrappers" >> uninstall.sh
 
 cd ..
 rm "${RELEASEDIR}.zip"
